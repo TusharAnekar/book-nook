5e5d4ad1ad9988f35b98b662d3f54e39c4a2ce18
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-17 17:22:13 +0530

    style- added some style changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Header } from "../../components/Header/Header";
 import { useContext } from "react";
 import { CategoriesContext } from "../../contexts/categories-context";
 import { Footer } from "../../components/Footer/Footer";
+import { Link } from "react-router-dom";
 
 export function Home() {
   const {
@@ -20,7 +21,7 @@ export function Home() {
             <div className="welcome-message-container">
               <p>Welcome to Book Nook,</p>
               <h1>For All Your Reading Needs</h1>
-              <button>SHOP NOW</button>
+              <Link to={"/products"} className="products-link">SHOP NOW</Link>
             </div>
           </div>
 
