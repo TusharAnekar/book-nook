5bc2e1a29443439d2ff8cb6ff7dadb30af547960
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-26 12:48:31 +0530

    feat- routes added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 import { Home } from "./pages/Home/Home";
 import { Products } from "./pages/Products/Products";
 import { Header } from "./components/Header/Header";
+import { Login } from "./pages/Login/Login";
+import { Signup } from "./pages/Signup/Signup";
+import { Wishlist } from "./pages/Wishlist/Wishlist";
+import { Cart } from "./pages/Cart/Cart";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/products" element={<Products/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/wishlist" element={<Wishlist/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
       </Routes>
     </div>
   );
