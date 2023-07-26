9cea725c02dad7363abcbdadd623748c39b0fb86
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-26 12:49:22 +0530

    feat- providers added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { CategoriesProvider } from "./contexts/categories-context";
 import { ProductsProvider } from "./contexts/products-context";
+import { AuthProvider } from "./contexts/auth-context";
+import { WishlistProvider } from "./contexts/wishlist-context";
+import { CartProvider } from "./contexts/cart-context";
 
 // Call make Server
 makeServer();
@@ -13,11 +16,17 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductsProvider>
-        <CategoriesProvider>
-          <App />
-        </CategoriesProvider>
-      </ProductsProvider>
+      <AuthProvider>
+        <ProductsProvider>
+          <CategoriesProvider>
+            <WishlistProvider>
+              <CartProvider>
+                <App />
+              </CartProvider>
+            </WishlistProvider>
+          </CategoriesProvider>
+        </ProductsProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
