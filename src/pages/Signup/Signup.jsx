22c44a746921d00c4c9cb4d70dcbeeee6765935b
Tushar Anekar <anekar.tushar@gmail.com>
2024-01-01 15:20:31 +0530

    feat- confirm password

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -5,6 +5,7 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/auth-context";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { toast } from "react-toastify";
 
 export function Signup() {
   const [signupDetails, setSignupDetails] = useState({
@@ -12,8 +13,10 @@ export function Signup() {
     lastName: "",
     email: "",
     password: "",
+    confirmPassword: "",
   });
   const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isShowConfirmPassword, setIsShowConfirmPassword] = useState(false);
 
   const { signupHandler } = useContext(AuthContext);
 
@@ -23,18 +26,26 @@ export function Signup() {
 
   function handleSignupSubmit(e) {
     e.preventDefault();
-    signupHandler(signupDetails);
-    setSignupDetails({
-      ...signupDetails,
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    });
+    if (signupDetails.password === signupDetails.confirmPassword) {
+      signupHandler(signupDetails);
+      setSignupDetails({
+        ...signupDetails,
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      });
+    } else {
+      toast.error("Passwords don't match.");
+    }
   }
 
   const handleShowPassword = () => {
     setIsShowPassword((prev) => !prev);
+  };
+
+  const handleShowConfirmPassword = () => {
+    setIsShowConfirmPassword((prev) => !prev);
   };
 
   const handleDropCopyPaste = (e) => {
@@ -108,6 +119,34 @@ export function Signup() {
               <VisibilityIcon
                 className="visibility-icon"
                 onClick={handleShowPassword}
+              />
+            )}
+          </label>
+          <label className="block login-password">
+            Confirm Password
+            <input
+              type={isShowConfirmPassword ? "text" : "password"}
+              placeholder="********"
+              name="confirmPassword"
+              value={signupDetails.confirmPassword}
+              pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+              title="Password should contain min 8 charcters, at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character"
+              required
+              onChange={handleInput}
+              onPaste={handleDropCopyPaste}
+              onDrop={handleDropCopyPaste}
+              onCopy={handleDropCopyPaste}
+              className="block input"
+            />
+            {isShowConfirmPassword ? (
+              <VisibilityOffIcon
+                className="visibility-icon"
+                onClick={handleShowConfirmPassword}
+              />
+            ) : (
+              <VisibilityIcon
+                className="visibility-icon"
+                onClick={handleShowConfirmPassword}
               />
             )}
           </label>
